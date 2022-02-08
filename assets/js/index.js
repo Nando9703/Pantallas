ae7c5f60bc2d02404053f8d63192bf28425f0c19
Nando9703 <82744092+Nando9703@users.noreply.github.com>
2022-02-08 08:18:25 -0500

    Login agregado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,11 @@
 function login(){
     var password = document.getElementById("password");
     var user = document.getElementById("user");
-    if (password.value==1997 && user.value=='Nando'){ 
-        window.location="index.html";
+    if (password.value=='1997' && user.value=='Nando'){ 
+        window.location.assign("index.html");
         } 
         else{ 
              alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
         } 
     } 
+    
